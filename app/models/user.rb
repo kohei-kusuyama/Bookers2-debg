22deb24aa7a2dfcdee6c37f4ee189e6cfb3d538c
KoheiKusuyama <144124225+kohei-kusuyama@users.noreply.github.com>
2023-11-09 08:30:14 +0000

    [finish]231109_search

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,19 @@ def following?(user)
   followings.include?(user)
 end
 
+# 検索
+def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+
   # belongs_to :books
 
   has_many :books, dependent: :destroy
